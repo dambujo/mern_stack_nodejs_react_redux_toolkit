7f66aa0f8d52832505b6d280b00712066083d15a
Antonio Dambujo <pdambuja@gmail.com>
2022-05-17 09:14:43 +0200

    goals controller and routes setup

--- backend/src/controllers/goalController.js
+++ backend/src/controllers/goalController.js
@@ -1,14 +1,23 @@
-const getGoals = (req, res) => {
+import asynHandler from 'express-async-handler';
+
+const getGoals = asynHandler(async (req, res) => {
   res.status(200).json({ message: 'Get All Goals' });
-};
-const createGoal = (req, res) => {
-  res.status(201).json({ message: 'Set goals' });
-};
-const updateGoal = (req, res) => {
+});
+
+const createGoal = asynHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error('Please add a text field');
+  }
+  res.status(200).json({ message: 'Set Goal' });
+});
+
+const updateGoal = asynHandler(async (req, res) => {
   res.status(200).json({ message: `Update goal ${req.params.id}` });
-};
-const deletGoal = (req, res) => {
+});
+
+const deletGoal = asynHandler(async (req, res) => {
   res.status(200).json({ message: `Delete goal ${req.params.id}` });
-};
+});
 
 export default { getGoals, createGoal, updateGoal, deletGoal };
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,11 +2,17 @@ import express from 'express';
 //const dotenv = require('dotenv').config();
 import 'dotenv/config';
 import v1Router from './v1/routes/index.js';
+//import errorHandler from './middleware/errorMiddleware.js';
 
 const port = process.env.API_PORT || 5000;
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use('/', v1Router);
+//app.use(errorHandler);
 
 app.listen(port, () =>
   console.log(`Backend Server is running on port ${port}`),
